f607e8cbb02ce48ccdb047fc7fbde4edebf193b1
Hemanth <hemanthcse1@gmail.com>
2021-10-01 18:46:05 +0530

    singly linked list implementation using Java

---
+++ src/main/java/linkedList/EmployeeLinkedList.java
@@ -0,0 +1,47 @@
+package linkedList;
+
+public class EmployeeLinkedList {
+
+    private EmployeeNode head;
+    private int size = 0;
+
+    public void addToFront(Employee employee){
+        EmployeeNode node = new EmployeeNode(employee);
+        node.setNext(head);
+        head = node;
+        size++;
+    }
+
+    public EmployeeNode removeFromFront(){
+        if (isEmpty()){
+            return null;
+        }
+
+        EmployeeNode removeNode = head;
+        head = head.getNext();
+        size--;
+        removeNode.setNext(null);
+        return removeNode;
+    }
+
+
+    public void printList(){
+        EmployeeNode current = head;
+        System.out.print("Head -> ");
+        while (current != null){
+            System.out.print(current);
+            System.out.print(" -> ");
+            current = current.getNext();
+        }
+
+        System.out.println(" null ");
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+}
---
+++ src/main/java/linkedList/Main.java
@@ -0,0 +1,32 @@
+package linkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee hemanth= new Employee("Hemanth","Kumar",101);
+        Employee anil = new Employee("Anil","Kumar",102);
+        Employee kiran = new Employee("Kiran","Kumar",103);
+        Employee tejas = new Employee("Tejas","Kumar",104);
+
+
+        EmployeeLinkedList employeeLinkedList = new EmployeeLinkedList();
+
+        System.out.println(employeeLinkedList.isEmpty());
+
+        employeeLinkedList.addToFront(hemanth);
+        employeeLinkedList.addToFront(anil);
+        employeeLinkedList.addToFront(kiran);
+        employeeLinkedList.addToFront(tejas);
+
+        System.out.println(employeeLinkedList.getSize());
+
+
+        employeeLinkedList.printList();
+
+        employeeLinkedList.removeFromFront();
+        System.out.println(employeeLinkedList.getSize());
+        employeeLinkedList.printList();
+
+
+    }
+}
